002516da25627b3b081329bf1d1b0811f779e4ce
pankov <pankov@tutu.tech>
2024-11-18 16:35:16 +0300

    alarm timeout

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,22 @@
 #include <WiFi.h>
 
 #define ALARM_PIN 19
+#define MAX_ALARM_DURATION_IN_MSEC 60000
 
 typedef struct Message {
     bool alarm;
 } Message;
 
 bool currentAlarmState;
+int alarmStartedAt;
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
     auto data = (Message *) incomingData;
     if (currentAlarmState == data->alarm) {
         return;
     }
     currentAlarmState = data->alarm;
-    Serial.print("alarm");
-    Serial.println(currentAlarmState);
     if (data->alarm) {
+        alarmStartedAt = millis();
         digitalWrite(ALARM_PIN, LOW);
     } else {
         digitalWrite(ALARM_PIN, HIGH);
@@ -38,4 +39,8 @@ void setup() {
 }
 
 void loop() {
+    if (currentAlarmState && (millis() - alarmStartedAt) > MAX_ALARM_DURATION_IN_MSEC) {
+        currentAlarmState = false;
+        digitalWrite(ALARM_PIN, HIGH);
+    }
 }
