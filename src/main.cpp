90e0353785802406f676ab90513a33c5a593806a
pankov <pankov@tutu.tech>
2024-12-06 19:27:14 +0300

    ping

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include "Ticker.h"
 
 #define ALARM_PIN 19
 #define MAX_ALARM_DURATION_IN_MSEC 60000
 
+SemaphoreHandle_t mutex = xSemaphoreCreateMutex();
+
 typedef struct Message {
     bool alarm;
+    bool ping;
 } Message;
 
-SemaphoreHandle_t mutex = xSemaphoreCreateMutex();
+class Semaphore {
+public:
+    static bool take() {
+        return xSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE;
+    }
+
+    static bool give() {
+        return xSemaphoreGive(mutex);
+    }
+};
+
+Ticker ticker;
 bool alarmState;
 int alarmStartedAt;
 
@@ -24,19 +39,37 @@ void setAlarmState(bool state) {
 
 
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
-    if (xSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE) {
-        auto data = (Message *) incomingData;
+    auto data = (Message *) incomingData;
+    if (data->ping) {
+        ticker.once_ms(1000, []() {
+            if (Semaphore::take()) {
+                for (auto i = 0; i < 3; i++) {
+                    digitalWrite(ALARM_PIN, LOW);
+                    delay(50);
+                    digitalWrite(ALARM_PIN, HIGH);
+                    delay(1000);
+                }
+                Semaphore::give();
+            }
+        });
+    }
+
+    if (Semaphore::take()) {
+        Serial.println(alarmState);
+        Serial.println(data->alarm);
         if (alarmState == data->alarm) {
-            xSemaphoreGive(mutex);
+            Semaphore::give();
             return;
         }
+
         setAlarmState(data->alarm);
         if (alarmState) {
             digitalWrite(ALARM_PIN, LOW);
         } else {
             digitalWrite(ALARM_PIN, HIGH);
         }
-        xSemaphoreGive(mutex);
+
+        Semaphore::give();
     }
 }
 
@@ -53,11 +86,11 @@ void setup() {
 }
 
 void loop() {
-    if (xSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE) {
+    if (Semaphore::take()) {
         if (alarmState && (millis() - alarmStartedAt) > MAX_ALARM_DURATION_IN_MSEC) {
             alarmState = false;
             digitalWrite(ALARM_PIN, HIGH);
         }
-        xSemaphoreGive(mutex);
+        Semaphore::give();
     }
 }
